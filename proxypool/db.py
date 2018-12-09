65bdcbb8e319f942eee1d77ee9dd459e9e7c0609
hamdi <javs_shao@163.com>
2018-12-09 16:26:33 +0800

    代理分数配置

--- proxypool/db.py
+++ proxypool/db.py
@@ -1,2 +1,3 @@
 import redis
 from proxypool.error import PoolEmptyError
+from proxypool.settings import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, REDIS_KEY
--- proxypool/settings.py
+++ proxypool/settings.py
@@ -1,10 +1,12 @@
 # Redis数据库地址
 REDIS_HOST = '127.0.0.1'
-
 # Redis端口
 REDIS_PORT = 6379
-
 # Redis密码，如无填None
 REDIS_PASSWORD = None
-
 REDIS_KEY = 'proxie_s'
+
+# 代理分数
+MAX_SCORE = 100
+MIN_SCORE = 0
+INITIAL_SCORE = 10
